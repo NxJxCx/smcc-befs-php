554aaf4b1ed51f469fa6c68d21efb564d38ab318
Neil Jason Cañete <96198550+NxJxCx@users.noreply.github.com>
2025-04-15 02:33:23 +0800

    bug fix 102

--- app/dean/dean_recommended_sc.php
+++ app/dean/dean_recommended_sc.php
@@ -7,9 +7,6 @@ $stud_id=conn()->sanitize($_REQUEST['stud_id']);
 $query="update students set level = 'PREBOARD2' where id = '$stud_id'" or die(mysqli_error(conn()->get_conn()));	  
 if (conn()->query($query)) 
 {
-			echo "<script type='text/javascript'>window.alert('Updated');
-</script>";
-
-			header("location: dean_students_all");
+	echo "<script type='text/javascript'>window.alert('Updated'); window.location.href = \"dean_students_all\"</script>";
 }
 
