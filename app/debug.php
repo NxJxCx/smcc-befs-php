d199a018330189fdb617c322d3cad43f7b042e28
Neil Jason Cañete <96198550+NxJxCx@users.noreply.github.com>
2025-04-15 11:23:00 +0800

    onnx format

--- app/debug.php
+++ app/debug.php
@@ -4,7 +4,7 @@ echo "<code>$log</code>";
 
 $model_paths = __DIR__ . DIRECTORY_SEPARATOR . "inference";
 $inference_models = glob($model_paths . DIRECTORY_SEPARATOR . "*.onnx");
-$api_pages = array_map(fn($page) => "/inference/" . pathinfo(basename($page), PATHINFO_FILENAME), array_diff($inference_models, ['.', '..']));
+$api_pages = array_map(fn($page) => "/inference/" . pathinfo(basename($page), PATHINFO_FILENAME)  . ".onnx", array_diff($inference_models, ['.', '..']));
 echo "<br><br>Model Pages:<br>";
 foreach ($api_pages as $page) {
     echo "<a href='$page'>$page</a><br>";
