af8f01015f0f25d3543da5f2f65f0e8c10314421
Neil Jason Cañete <96198550+NxJxCx@users.noreply.github.com>
2025-04-15 11:15:17 +0800

    debug

--- app/debug.php
+++ app/debug.php
@@ -1,3 +1,11 @@
 <?php
 $log = (file_exists("debug.log")) ? file_get_contents("debug.log") : "No log file found.";
 echo "<code>$log</code>";
+
+$model_paths = __DIR__ . DIRECTORY_SEPARATOR . "inference";
+$inference_models = glob($model_paths . DIRECTORY_SEPARATOR . "*.onnx");
+$api_pages = array_map(fn($page) => "/inference/" . pathinfo(basename($page), PATHINFO_FILENAME), array_diff($inference_models, ['.', '..']));
+echo "<br><br>Model Pages:<br>";
+foreach ($api_pages as $page) {
+    echo "<a href='$page'>$page</a><br>";
+}
