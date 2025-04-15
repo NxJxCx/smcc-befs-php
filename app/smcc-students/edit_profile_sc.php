cde377edfadce4debd503718084a3d0a96f473eb
Neil Jason Cañete <96198550+NxJxCx@users.noreply.github.com>
2025-04-15 08:53:27 +0800

    removed or die()

--- app/smcc-students/edit_profile_sc.php
+++ app/smcc-students/edit_profile_sc.php
@@ -2,15 +2,13 @@
 
 authenticated_page("student");
 
-
-	$query="UPDATE students SET logged_in = 'NO' WHERE id = '" . user_id() . "'" or die(mysqli_error(conn()->get_conn()));	  
-	if (conn()->query($query)) 
-	{
-        echo "<script type='text/javascript'>alert('You have logged out!')";
-        header("Location: " . base_url() . "/student/students_profile");
-	} 
-	else 
-	{
-		echo "Error: " . $query . "<br>" . mysqli_error(conn()->get_conn());
-	}
-?>
+$query = "UPDATE students SET logged_in = 'NO' WHERE id = '" . user_id() . "'";	  
+if (conn()->query($query)) 
+{
+	echo "<script type='text/javascript'>alert('You have logged out!')";
+	header("Location: " . base_url() . "/student/students_profile");
+} 
+else 
+{
+	echo "Error: " . $query . "<br>" . mysqli_error(conn()->get_conn());
+}
